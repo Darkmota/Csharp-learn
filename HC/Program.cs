79f5c16db043973991218ce0507645bdca631e81
omegaPi <578146845@qq.com>
2018-09-26 11:31:09 +0800

    9.26

--- HC/Program.cs
+++ HC/Program.cs
@@ -12,7 +12,7 @@ namespace HC
         {
             long time = DateTime.Now.Ticks;
             int primeCount = 0;
-            for (int i = 2; i <= 100000; ++i)
+            for (int i = 2; i <= 1000000; ++i)
             {
                 bool isp = true;
                 for (int j = 2; j * j <= i; ++j)
@@ -28,19 +28,18 @@ namespace HC
                     primeCount++;
                 }
             }
-            Console.WriteLine("Simple-1[" + primeCount + "]:" + (DateTime.Now.Ticks - time) / 10000.0 + "ms");
-            Console.ReadKey();
+            Console.WriteLine("Simple-1[" + primeCount + "][1~1000000]:" + (DateTime.Now.Ticks - time) / 10000.0 + "ms");
         }
         static void countPrime2()
         {
             long time = DateTime.Now.Ticks;
-            int[] primeList = new int[10001];
-            bool[] primeMap = new bool[100001];
+            int[] primeList = new int[1000001];
+            bool[] primeMap = new bool[1000001];
             Array.Clear(primeMap, 0, primeMap.Length);
             int primeCount = 1;
             primeList[0] = 2;
             primeMap[2] = true;
-            for (int i = 3; i <= 100000; i += 2)
+            for (int i = 3; i <= 1000000; i += 2)
             {
                 bool isp = true;
                 for (int j = 0; j < primeCount && primeList[j] * primeList[j] <= i; ++j)
@@ -57,8 +56,64 @@ namespace HC
                     primeList[primeCount++] = i;
                 }
             }
-            Console.WriteLine("List&Map-2[" + primeCount + "]:" + (DateTime.Now.Ticks - time) / 10000.0 + "ms");
-            Console.ReadKey();
+            Console.WriteLine("List&Map-2[" + primeCount + "][1~1000000]:" + (DateTime.Now.Ticks - time) / 10000.0 + "ms");
+        }
+        static void formatIt(string poem)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                for (int j = 0; j < 8; ++j)
+                {
+                    Console.Write(poem[i * 8 + j] + "     ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void time()
+        {
+            Console.WriteLine(DateTime.Now.ToString());
+            Console.WriteLine(DateTime.Now.ToString("d"));
+            Console.WriteLine(DateTime.Now.ToString("D"));
+            Console.WriteLine(DateTime.Now.ToString("f"));
+            Console.WriteLine(DateTime.Now.ToString("F"));
+            Console.WriteLine(DateTime.Now.ToString("g"));
+            Console.WriteLine(DateTime.Now.ToString("G"));
+            Console.WriteLine(DateTime.Now.ToString("t"));
+            Console.WriteLine(DateTime.Now.ToString("T"));
+            Console.WriteLine(DateTime.Now.ToString("u"));
+            Console.WriteLine(DateTime.Now.ToString("U"));
+            Console.WriteLine(DateTime.Now.ToString("m"));
+            Console.WriteLine(DateTime.Now.ToString("M"));
+            Console.WriteLine(DateTime.Now.ToString("r"));
+            Console.WriteLine(DateTime.Now.ToString("R"));
+            Console.WriteLine(DateTime.Now.ToString("y"));
+            Console.WriteLine(DateTime.Now.ToString("Y"));
+            Console.WriteLine(DateTime.Now.ToString("o"));
+            Console.WriteLine(DateTime.Now.ToString("O"));
+            Console.WriteLine(DateTime.Now.ToString("s"));
+        }
+        static void IP()
+        {
+            Console.WriteLine("Input IP:");
+            string s = Console.ReadLine();
+            string[] ss = s.Split('.');
+            if (ss.Length != 4)
+            {
+                Console.WriteLine("Invalid IP!");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < 4; ++i)
+                {
+                    if (Convert.ToInt32(ss[i]) < 0 || Convert.ToInt32(ss[i]) > 255)
+                    {
+                        Console.WriteLine("Invalid IP!");
+                        return;
+                    }
+                }
+                Console.WriteLine("Fine.");
+            }
         }
         static void Main(string[] args)
         {
@@ -91,6 +146,16 @@ namespace HC
             // Prime
             countPrime1();
             countPrime2();
+            Console.ReadKey();
+
+            formatIt("日照香炉生紫烟，遥望瀑布挂前川。飞流直下三千尺，疑是银河落九天。");
+            Console.ReadKey();
+
+            time();
+            Console.ReadKey();
+
+            IP();
+            Console.ReadKey();
         }
     }
 }
---
+++ HC3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HC3
+{
+    class Program
+    {
+        static void StudentName()
+        {
+            string[] names = new string[] { "A", "B", "C", "D" };
+            for (int i = 0; i < names.Length; ++i)
+            {
+                Console.WriteLine(names[i] + "  ");
+            }
+            Console.WriteLine("Change B to E");
+            names[1] = "E";
+            for (int i = 0; i < names.Length; ++i)
+            {
+                Console.WriteLine(names[i] + "  ");
+            }
+
+        }
+        static void Dic()
+        {
+            Dictionary<string, string[]> d = new Dictionary<string, string[]>();
+            d.Add("A", new string[] { "A0, A1" });
+            d.Add("B", new string[] { "B0, B1" });
+            d.Add("C", new string[] { "C0, C1" });
+            string s;
+            Console.WriteLine("Input(A/B/C): ");
+            s = Console.ReadLine();
+            try {
+                for (int i = 0; i < d[s].Length; ++i)
+                {
+                    Console.WriteLine("  Output: " + d[s][i]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        static void Random()
+        {
+            int[,] list = new int[4, 10];
+            Random seed = new Random();
+            for (int i = 0; i < 10; ++i)
+            {
+                for (int j = 0; j < 4; ++j)
+                {
+                    list[i, j] = seed.Next(0, 100);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            StudentName();
+            Console.ReadKey();
+
+            Dic();
+            Console.ReadKey();
+        }
+    }
+}
